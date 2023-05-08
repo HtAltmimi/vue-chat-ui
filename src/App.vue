843cde112d10d8be88d9a00376dc8ab5d94a0294
unknown <alithamir1999@gmail.com>
2023-05-08 11:03:55 +0300

    first commit:

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import ChatList from "@/components/ChatList.vue";
+import { ref } from "vue";
+const conversations = ref([]);
+conversations.value.push({
+  title: "Chat with Ahmed",
+  messages: [
+    {
+      outgoing: true,
+      text: "Hi , Ahmed ",
+      username: "ahmed1998",
+    },
+    {
+      outgoing: false,
+      text: "Hello, Ali!",
+      username: "hhh",
+    },
+  ],
+});
+conversations.value.push({
+  title: "Chat with Krar",
+  messages: [
+    {
+      outgoing: true,
+      text: "Hi, Krar",
+      username: "hdrm147",
+    },
+    {
+      outgoing: false,
+      text: "Hello, Ali!",
+      username: "azeez",
+    },
+  ],
+});
+</script>
+<template>
+  <div class="container flex justify-center">
+    <chat-list :conversations="conversations"></chat-list>
+  </div>
+</template>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="bg-indigo-500 rounded px-2 py-1 font-bold text-white hover:bg-indigo-600"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+    default: "Click Me ",
+  },
+});
+</script>
